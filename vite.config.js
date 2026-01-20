a6f21515c94d438bb7486fdc50a306280d71bf16
arthxrrr <arthurhenriquerech@gmail.com>
2026-01-20 11:50:39 -0300

    agora vai

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // 👈 ESSENCIAL para GitHub Pages
   plugins: [
     vue({
       template: {
         compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('Tres') && tag !== 'TresCanvas',
+          isCustomElement: (tag) =>
+            tag.startsWith('Tres') && tag !== 'TresCanvas',
         },
       },
     }),
